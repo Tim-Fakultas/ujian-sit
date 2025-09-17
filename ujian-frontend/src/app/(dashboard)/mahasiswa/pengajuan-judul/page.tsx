80175665d01da334bac2b16897575e11a52d7238
abdodyssey <abdodyssey@gmail.com>
2025-09-17 22:58:57 +0700

    woi2

--- ujian-frontend/src/app/(dashboard)/mahasiswa/pengajuan-judul/page.tsx
+++ ujian-frontend/src/app/(dashboard)/mahasiswa/pengajuan-judul/page.tsx
@@ -1,10 +1,13 @@
-import { DataTable } from '@/components/data-table';
-import data from '@/lib/data.json'
+import { TablePengajuan } from "@/components/table-pengajuan";
 
 export default function MahasiswaPage() {
   return (
-    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-      <DataTable data={data} />
+    <div className="container mx-auto py-8 px-6">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold text-balance">Sistem E-Skripsi</h1>
+        <p className="text-muted-foreground mt-2">Kelola pengajuan skripsi mahasiswa dengan mudah</p>
+      </div>
+      <TablePengajuan/>
     </div>
   );
 }
